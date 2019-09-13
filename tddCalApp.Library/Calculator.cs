5abe83535f38a2052a06f040e43ebc87ca764ec9
ujjwalsharma9 <ujjwalsharma9671@gmail.com>
2019-09-12 21:41:28 -0700

    First Commit of Calc App

---
+++ tddCalApp.Library/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tddCalApp.Library
+{
+    class Calculator
+    {
+        //Function to Add 2 numbers
+        public int add(int num1,int num2) 
+        {
+            return 0;
+        }
+    }
+}
