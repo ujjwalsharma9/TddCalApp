44d6f19bad1d918953cf2acbe995ac72c92e241b
ujjwalsharma9 <ujjwalsharma9671@gmail.com>
2019-09-12 22:13:34 -0700

    2 Unit Test Passed

--- TddCalApp.Test/UnitTest1.cs
+++ TddCalApp.Test/UnitTest1.cs
@@ -18,5 +18,18 @@ namespace TddCalApp.Test
             Assert.AreEqual(7, result, "Test two Integers 3 and 4");
            
         }
+
+
+        [TestMethod]
+        public void ShouldReturnZeroonPassingEqualMagnitudeandEqualSignedNumbers()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = 3;
+            int num2 = -3;
+            int result;
+            result = testCalculator.add(num1, num2);
+            Assert.AreEqual(0, result, "Test two Integers 3 and 4");
+
+        }
     }
 }
